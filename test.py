9d3772fc29ff716316420a2e1eb9a41b3648eb32
Yun-shao Sung <yss265@login-0-2.local>
2015-10-29 14:39:54 -0400

    current version on hpc

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ from pylab import plt
 from scipy.signal import butter, lfilter, freqz
 
 
-samples_scattered = pickle.load( open( "./data/data_scattered_noLowPassed.in", "rb" ) )
+samples_scattered = pickle.load( open( "./data/Data_scattered_lowPassed_energy.in", "rb" ) )
 
-samples = pickle.load( open( "./data/data_small.in", "rb" ) )
+samples = pickle.load( open( "./data/data_small_correctFormat.in", "rb" ) )
 
 print samples['jazz'][0]
 print samples_scattered['jazz'][0]
