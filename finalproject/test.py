dcd5ce8279e18d7fc297a01f0ad6973df21654ac
GabrielWen <gabriel.weng@gmail.com>
2015-12-05 15:53:02 -0500

    Restore

--- finalproject/test.py
+++ finalproject/test.py
@@ -1,6 +1,6 @@
 import subprocess
 
 for numDivide in xrange(1, 6):
-  #for k in xrange(100, 4100, 100):
-  for batch in xrange(1, 5):
-    subprocess.call('qsub workPipeLine.pbs -v n={0},k={1},b={2}'.format(numDivide, 100, batch / 10.0), shell=True)
+  for k in xrange(100, 4100, 100):
+    for batch in xrange(1, 5):
+      subprocess.call('qsub workPipeLine.pbs -v n={0},k={1},b={2}'.format(numDivide, 100, batch / 10.0), shell=True)
