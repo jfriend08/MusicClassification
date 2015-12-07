3b86102210593e1172a38227844cadac07c04984
jfriend08 <petersungjfriend08@gmail.com>
2015-12-06 20:30:55 -0500

    add results

--- finalproject/test.py
+++ finalproject/test.py
@@ -3,6 +3,6 @@ import subprocess
 for numDivide in xrange(1, 2):
   for k in xrange(100, 4000, 100):
     for batch in xrange(1, 6):
-      for n_neighbors in xrange(1, 6):
+      for n_neighbors in xrange(6, 10):
         jobName = '{0}_{1}_{2}_{3}'.format(numDivide, k, batch / 10.0, n_neighbors)
         subprocess.call('qsub workPipeLine.pbs -N {0} -v n={1},k={2},b={3},nei={4}'.format(jobName, numDivide, k, batch / 10.0, n_neighbors), shell=True)
